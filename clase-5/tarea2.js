6da0b859df1b534bd7e9ae8884be221d2d371f06
Mauricioam <mauricioam91@gmail.com>
2022-03-09 12:21:25 -0300

    corrección Tarea 2.js
    Todas las variables cambie a camelCase y minúsculas.

--- clase-5/tarea2.js
+++ clase-5/tarea2.js
@@ -1,43 +1,43 @@
 const $calcular = document.querySelector("#boton-calcular")
 
 $calcular.onclick = function (){
-    const Horas$1 = Number(document.querySelector(".horas-1").value);
-    const Horas$2 = Number(document.querySelector(".horas-2").value);
-    const Horas$3 = Number(document.querySelector(".horas-3").value);
-    const Horas$4 = Number(document.querySelector(".horas-4").value); 
-    const Horas$5 = Number(document.querySelector(".horas-5").value);
+    const horasVideo1 = Number(document.querySelector(".horas-1").value);
+    const horasVideo2 = Number(document.querySelector(".horas-2").value);
+    const horasVideo3 = Number(document.querySelector(".horas-3").value);
+    const horasVideo4 = Number(document.querySelector(".horas-4").value); 
+    const horasVideo5 = Number(document.querySelector(".horas-5").value);
 
-     TotalHoras = Horas$1 + Horas$2 + Horas$3 + Horas$4 + Horas$5;
+     totalHoras = horasVideo1 + horasVideo2 +horasVideo3 + horasVideo4 + horasVideo5;
 
-    const Minutos$1 = Number(document.querySelector(".minutos-1").value);
-    const Minutos$2 = Number(document.querySelector(".minutos-2").value);
-    const Minutos$3 = Number(document.querySelector(".minutos-3").value);
-    const Minutos$4 = Number(document.querySelector(".minutos-4").value); 
-    const Minutos$5 = Number(document.querySelector(".minutos-5").value);
+    const minutosVideo1 = Number(document.querySelector(".minutos-1").value);
+    const minutosVideo2 = Number(document.querySelector(".minutos-2").value);
+    const minutosVideo3 = Number(document.querySelector(".minutos-3").value);
+    const minutosVideo4 = Number(document.querySelector(".minutos-4").value); 
+    const minutosVideo5 = Number(document.querySelector(".minutos-5").value);
 
-    let TotalMinutos = Minutos$1 + Minutos$2 + Minutos$3 + Minutos$4 + Minutos$5;
+    let totalMinutos = minutosVideo1 + minutosVideo2 + minutosVideo3 + minutosVideo4 + minutosVideo5;
 
-    const Segundos$1 = Number(document.querySelector(".segundos-1").value);
-    const Segundos$2 = Number(document.querySelector(".segundos-2").value);
-    const Segundos$3 = Number(document.querySelector(".segundos-3").value);
-    const Segundos$4 = Number(document.querySelector(".segundos-4").value); 
-    const Segundos$5 = Number(document.querySelector(".segundos-5").value);
+    const segundosVideo1 = Number(document.querySelector(".segundos-1").value);
+    const segundosVideo2 = Number(document.querySelector(".segundos-2").value);
+    const segundosVideo3 = Number(document.querySelector(".segundos-3").value);
+    const segundosVideo4 = Number(document.querySelector(".segundos-4").value); 
+    const segundosVideo5 = Number(document.querySelector(".segundos-5").value);
 
-    let TotalSegundos = Segundos$1 + Segundos$2 + Segundos$3 + Segundos$4 + Segundos$5;
+    let totalSegundos = segundosVideo1 + segundosVideo2 + segundosVideo3 + segundosVideo4 + segundosVideo5;
 
-    if (TotalMinutos >= 60){
-        let division = TotalMinutos % 60;
-         TotalHoras +=  (TotalMinutos-division)/ 60;
-         TotalMinutos = division;
+    if (totalMinutos >= 60){
+        let division = totalMinutos % 60;
+         totalHoras +=  (totalMinutos-division)/ 60;
+         totalMinutos = division;
 
     }
-    if (TotalSegundos >= 60){
-        let division1 = TotalSegundos % 60;
-        TotalMinutos += (TotalSegundos- division1)/60;
-        TotalSegundos = division1;
+    if (totalSegundos >= 60){
+        let division1 = totalSegundos % 60;
+        totalMinutos += (totalSegundos - division1)/60;
+        totalSegundos = division1;
     }
     const $TextoMostrado = document.querySelector("#texto-mostrado");
-    $TextoMostrado.innerText = `El tiempo total es de ${TotalHoras} horas , ${TotalMinutos} minutos y ${TotalSegundos} segundos`;
+    $TextoMostrado.innerText = `El tiempo total es de ${totalHoras} horas , ${totalMinutos} minutos y ${totalSegundos} segundos`;
     return false;
     }
 
